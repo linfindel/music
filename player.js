658ee8944b7ad4e67e0311b4660c3a29e236309d
Linfindel <linfindel@gmail.com>
2024-02-10 17:21:14 +0000

    Bug fix for Firefox

--- player.js
+++ player.js
@@ -356,7 +356,14 @@ function changeSkew(newSkew) {
 function uploadFile() {
   let input = document.createElement('input');
   input.type = 'file';
-  input.accept = 'audio/mp3, audio/wav, audio/ogg';
+
+  if (navigator.userAgent.toLowerCase().includes('firefox')) {
+    input.accept = 'audio/*';  
+  }
+
+  else {
+    input.accept = 'audio/mp3, audio/wav, audio/ogg';
+  }
   input.onchange = () => {
     let file = input.files[0];
     if (file) {
